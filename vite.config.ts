5dcc2d8c8bf1b7f06aeb34dc9fe6bec73454b6c2
SohanSonu303 <87355196+SohanSonu303@users.noreply.github.com>
2024-08-17 19:15:17 +0530

    changes package files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Listen on all network interfaces
+    port: 5173,      // Port to listen on (should match EXPOSE in Dockerfile)
+    strictPort: true // Ensures Vite doesn't pick another port if 5173 is taken
+  }
+});
